e2f69306aa8526a7bf2a822ac44589d3abc5449a
mcgeeca <mcgeeca@pg.com>
2017-12-13 23:25:43 -0500

    DONE

--- src/m3_implementing_functions_that_mutate.py
+++ src/m3_implementing_functions_that_mutate.py
@@ -3,7 +3,7 @@ This module lets you practice IMPLEMENTING
 functions that MUTATE their arguments.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Craig McGee Jr.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
@@ -145,7 +145,9 @@ def swap_colors(circle, rectangle):
     #      B = temp
     ####################################################################
     # ------------------------------------------------------------------
-
+    circle_color = circle.fill_color
+    circle.fill_color = rectangle.fill_color
+    rectangle.fill_color = circle_color
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
